29e35cecd34f149baf6a744b4218e947d427df30
andy <andrew.w.pinson@gmail.com>
2020-11-08 23:05:45 -0800

    Fixing bug where new versions of lambda functions break "live" deployments because the lambda permission is tied to the version.  Removing the qualifier from the lambda permission.

--- lambda_permission.tf
+++ lambda_permission.tf
@@ -16,8 +16,8 @@ resource "aws_lambda_permission" "lambdas" {
   event_source_token = var.lambda_permission_event_source_token
   function_name      = regex("function:([^:\\/]*)", each.value.integration.uri)[0]
   principal           = var.lambda_permission_principal
-  qualifier           = length(regexall("function:[^:\\/]*:\\d+", each.value.integration.uri)) == 0 ? null : regex("function:[^:\\/]*\\:(\\d+)", each.value.integration.uri)[0]
   source_account      = var.lambda_permission_source_account
   source_arn          = var.lambda_permission_source_arn
   statement_id_prefix = var.lambda_permission_statement_id_prefix
-  }
+}
+
